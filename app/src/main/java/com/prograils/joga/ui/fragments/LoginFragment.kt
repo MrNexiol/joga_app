270408c32f6d3d09aaf722862dd030312a507207
Tomasz Kopyciński <tkopycinski@prograils.com>
2020-12-17 08:49:48 +0100

    Initial commit with navigation support

---
+++ app/src/main/java/com/prograils/joga/ui/fragments/LoginFragment.kt
@@ -0,0 +1,50 @@
+package com.prograils.joga.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import com.prograils.joga.R
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class LoginFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        val loginButton = view.findViewById<Button>(R.id.login_button)
+        loginButton.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToHomeFragment()
+            view.findNavController().navigate(action)
+        }
+        return view
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            LoginFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
