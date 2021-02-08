c0bcc21a1834417fed84f0935a9b4fae500bc7c8
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-02-08 10:59:34 +0100

    Added video url to instructor model

--- app/src/main/java/com/prograils/joga/api/Instructor.kt
+++ app/src/main/java/com/prograils/joga/api/Instructor.kt
@@ -1,7 +1,11 @@
 package com.prograils.joga.api
 
+import com.google.gson.annotations.SerializedName
+
 data class Instructor(
         val id: String,
         val avatar_url: String,
-        val name: String
+        val name: String,
+        @SerializedName("video_url")
+        val videoUrl: String
 )
