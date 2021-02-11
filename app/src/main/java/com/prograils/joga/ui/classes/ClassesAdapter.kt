c91f49ac4741f59564e00b6647650202b69d4aab
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-02-11 22:14:54 +0100

    Modified classesViewModel, fragment and adapter to use category request

--- app/src/main/java/com/prograils/joga/ui/classes/ClassesAdapter.kt
+++ app/src/main/java/com/prograils/joga/ui/classes/ClassesAdapter.kt
@@ -7,10 +7,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.prograils.joga.R
+import com.prograils.joga.api.Category
 import com.prograils.joga.api.Class
 import com.prograils.joga.databinding.ClassesRecyclerViewItemBinding
 
-class ClassesAdapter (private var data: List<Class>) : RecyclerView.Adapter<ClassesAdapter.ViewHolder>() {
+class ClassesAdapter (private var data: List<Category>) : RecyclerView.Adapter<ClassesAdapter.ViewHolder>() {
 
     class ViewHolder (
             val binding: ClassesRecyclerViewItemBinding) : RecyclerView.ViewHolder(binding.root)
@@ -23,22 +24,22 @@ class ClassesAdapter (private var data: List<Class>) : RecyclerView.Adapter<Clas
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         Glide.with(holder.itemView)
-                .load(data[position].thumbnailUrl)
+                .load(data[position].image)
                 .fallback(R.drawable.placeholder_image)
                 .transition(DrawableTransitionOptions.withCrossFade())
                 .into(holder.binding.classThumbnailImageView)
-        holder.binding.classNameTextView.text = data[position].title
-        holder.binding.root.setOnClickListener {
-            val action = ClassesFragmentDirections.actionClassesFragmentToClassDetailsFragment(data[position].id)
-            it.findNavController().navigate(action)
-        }
+        holder.binding.classNameTextView.text = data[position].name
+//        holder.binding.root.setOnClickListener {
+//            val action = ClassesFragmentDirections.actionClassesFragmentToClassDetailsFragment(data[position].id)
+//            it.findNavController().navigate(action)
+//        }
     }
 
     override fun getItemCount(): Int {
         return data.size
     }
 
-    fun setData(data: List<Class>){
+    fun setData(data: List<Category>){
         this.data = data
         notifyDataSetChanged()
     }
--- app/src/main/java/com/prograils/joga/ui/classes/ClassesViewModel.kt
+++ app/src/main/java/com/prograils/joga/ui/classes/ClassesViewModel.kt
@@ -3,19 +3,20 @@ package com.prograils.joga.ui.classes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.prograils.joga.Repository
+import com.prograils.joga.api.Category
 import com.prograils.joga.api.Class
 import com.prograils.joga.api.RefreshableSource
 import com.prograils.joga.api.Resource
 
 class ClassesViewModel(repository: Repository, token: String) : ViewModel() {
-    val classesWrapper = object : RefreshableSource<List<Class>>() {
-        override fun provideLiveData(): LiveData<Resource<List<Class>>> {
-            return repository.getClasses(token)
+    val categoriesWrapper = object : RefreshableSource<List<Category>>() {
+        override fun provideLiveData(): LiveData<Resource<List<Category>>> {
+            return repository.getCategories(token)
         }
     }
 
     fun refreshClasses(){
-        classesWrapper.refresh()
+        categoriesWrapper.refresh()
     }
 
     init {
