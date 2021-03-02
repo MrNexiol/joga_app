c99273f1f53dbb4b4ad04f8517c2d9107d1d11e8
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-03-02 10:33:07 +0100

    Modified login screen to use green background and app icon

--- app/src/main/java/com/prograils/joga/ui/home/HomeFragment.kt
+++ app/src/main/java/com/prograils/joga/ui/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -103,6 +104,9 @@ class HomeFragment : Fragment() {
                 binding.secondDivider.visibility = View.GONE
             }
         })
+
+        requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.background)
+        requireActivity().window.navigationBarColor = ContextCompat.getColor(requireContext(), R.color.background)
         return binding.root
     }
 
