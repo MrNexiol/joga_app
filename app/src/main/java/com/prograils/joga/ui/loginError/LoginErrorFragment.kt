f5121e2bedb47792520e1a0fd2fd248860534c98
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-03-03 11:16:06 +0100

    Fixed coloring on login error screen

--- app/src/main/java/com/prograils/joga/ui/loginError/LoginErrorFragment.kt
+++ app/src/main/java/com/prograils/joga/ui/loginError/LoginErrorFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
+import com.prograils.joga.R
 import com.prograils.joga.databinding.FragmentLoginErrorBinding
 
 class LoginErrorFragment : Fragment() {
@@ -17,6 +19,9 @@ class LoginErrorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentLoginErrorBinding.inflate(inflater, container, false)
+
+        requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.background)
+        requireActivity().window.navigationBarColor = ContextCompat.getColor(requireContext(), R.color.background)
         return binding.root
     }
 
