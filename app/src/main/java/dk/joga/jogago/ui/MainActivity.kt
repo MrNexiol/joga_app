448e35178db81933f307848f770435841a42a85c
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-03-22 11:16:32 +0100

    Configured new bottom nav bar

--- app/src/main/java/dk/joga/jogago/ui/MainActivity.kt
+++ app/src/main/java/dk/joga/jogago/ui/MainActivity.kt
@@ -2,11 +2,32 @@ package dk.joga.jogago.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import dk.joga.jogago.R
+import dk.joga.jogago.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainBottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.navigation_home -> {
+                    findNavController(binding.navHostFragment.id).navigate(R.id.action_global_homeFragment)
+                    true
+                }
+                R.id.navigation_classes -> {
+                    findNavController(binding.navHostFragment.id).navigate(R.id.action_global_classesFragment)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
