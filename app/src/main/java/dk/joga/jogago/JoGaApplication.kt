e93e7f104f8227f8199d7baca8936b556d64b11d
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-03-26 12:57:10 +0100

    Fixed appContainer in JoGaApplication

--- app/src/main/java/dk/joga/jogago/JoGaApplication.kt
+++ app/src/main/java/dk/joga/jogago/JoGaApplication.kt
@@ -3,10 +3,8 @@ package dk.joga.jogago
 import android.app.Application
 
 class JoGaApplication : Application() {
-    val appContainer = AppContainer
-
     override fun onCreate() {
         super.onCreate()
-        appContainer.init(this)
+        AppContainer.init(this)
     }
 }
