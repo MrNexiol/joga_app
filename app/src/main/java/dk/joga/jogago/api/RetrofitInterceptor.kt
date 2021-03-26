8565285993582cc745090bc6e5cd06e79815358c
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-03-26 12:13:43 +0100

    Refactored interceptor

--- app/src/main/java/dk/joga/jogago/api/RetrofitInterceptor.kt
+++ app/src/main/java/dk/joga/jogago/api/RetrofitInterceptor.kt
@@ -3,21 +3,20 @@ package dk.joga.jogago.api
 import android.content.Context
 import dk.joga.jogago.R
 import okhttp3.Interceptor
-import okhttp3.Request
 import okhttp3.Response
 
 class RetrofitInterceptor(val context: Context) : Interceptor {
-    private var TOKEN_BASE: String = "Bearer"
+    private var tokenBase = "Bearer "
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        val originalRequest: Request = chain.request()
+        val originalRequest = chain.request()
         val preferences = context.getSharedPreferences(context.getString(R.string.preferences_name), Context.MODE_PRIVATE)
 
         if (originalRequest.url().toString().contains("sessions") && originalRequest.method().equals("POST")) {
             return chain.proceed(originalRequest)
         }
         val tokenKey = preferences.getString(context.getString(R.string.saved_token_key), "")
-        val newRequest: Request = originalRequest.newBuilder().header("Authorization", "$TOKEN_BASE $tokenKey").build()
+        val newRequest = originalRequest.newBuilder().header("Authorization", tokenBase + tokenKey).build()
 
         return chain.proceed(newRequest)
     }
