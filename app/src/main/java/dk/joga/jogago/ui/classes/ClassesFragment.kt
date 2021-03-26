9a2263bde864fd5e55e45be74f7986f2bfcfd623
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-03-26 13:17:47 +0100

    Refactored classes fragment and viewModel, deleted unused factory

--- app/src/main/java/dk/joga/jogago/ui/classes/ClassesFragment.kt
+++ app/src/main/java/dk/joga/jogago/ui/classes/ClassesFragment.kt
@@ -5,9 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
-import dk.joga.jogago.AppContainer
 import dk.joga.jogago.GridSpacingItemDecoration
 import dk.joga.jogago.R
 import dk.joga.jogago.databinding.FragmentClassesBinding
@@ -15,17 +14,13 @@ import dk.joga.jogago.databinding.FragmentClassesBinding
 class ClassesFragment : Fragment() {
     private var _binding: FragmentClassesBinding? = null
     private val binding get() = _binding!!
-    private lateinit var viewModel: ClassesViewModel
-    private lateinit var viewModelFactory: ClassesViewModelFactory
+    private val viewModel: ClassesViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentClassesBinding.inflate(inflater, container, false)
-        viewModelFactory = ClassesViewModelFactory(AppContainer.repository)
-        viewModel = ViewModelProvider(this, viewModelFactory).get(ClassesViewModel::class.java)
-
         val recyclerView = binding.classesRecyclerView
         val adapter = ClassesAdapter(listOf())
         recyclerView.adapter = adapter
