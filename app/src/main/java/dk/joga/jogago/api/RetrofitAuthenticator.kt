8aec3dcc4cd202796e46e2a724d17484ce3f2187
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-06-06 18:47:30 +0200

    Removed logging in after authentication error

--- app/src/main/java/dk/joga/jogago/api/RetrofitAuthenticator.kt
+++ app/src/main/java/dk/joga/jogago/api/RetrofitAuthenticator.kt
@@ -10,11 +10,7 @@ import okhttp3.Route
 
 class RetrofitAuthenticator(val context: Context) : Authenticator {
     override fun authenticate(route: Route?, response: Response): Request? {
-        var updatedToken: String?
-        updatedToken = getUpdatedToken()
-        if (updatedToken == null) {
-            updatedToken = tryToLoginAgain()
-        }
+        val updatedToken: String? = getUpdatedToken()
         
         return if (updatedToken == null) {
             null
@@ -35,26 +31,6 @@ class RetrofitAuthenticator(val context: Context) : Authenticator {
                 with(preferences.edit()) {
                     this.putString(context.getString(R.string.saved_token_key), newToken)
                     this.putString(context.getString(R.string.saved_refresh_token_key), reloginData.data.refreshToken)
-                    this.commit()
-                }
-            }
-        }
-        return newToken
-    }
-
-    private fun tryToLoginAgain(): String? {
-        val preferences = context.getSharedPreferences(context.getString(R.string.preferences_name), Context.MODE_PRIVATE)
-        val username = preferences.getString(context.getString(R.string.saved_username), null)
-        val password = preferences.getString(context.getString(R.string.saved_password), null)
-        var newToken: String? = null
-        if (username != null && password != null) {
-            val response = AppContainer.repository.syncLogin(username, password)
-            if (response.status == Status.Success) {
-                newToken = response.data!!.token
-                with(preferences.edit()) {
-                    this.putString(context.getString(R.string.saved_user_id), response.data.userId)
-                    this.putString(context.getString(R.string.saved_token_key), response.data.token)
-                    this.putString(context.getString(R.string.saved_refresh_token_key), response.data.refreshToken)
                     this.commit()
                 }
             }
